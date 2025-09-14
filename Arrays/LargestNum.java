f3e993fe221f75c71e481e7e0a3aefa125cdc0a3
chandan9648 <chandankkumar156@gmail.com>
2025-09-14 19:27:22 +0530

    Remove unused class files and add LinearSearch implementation

---
+++ Arrays/LargestNum.java
@@ -0,0 +1,18 @@
+
+public class LargestNum {
+
+    public static int getLargest(int numbers[]) {
+        int largest = Integer.MIN_VALUE; //-infinity
+
+        for (int i=0; i < numbers.length; i++) {
+            if (largest < numbers[i]) {
+                largest = numbers[i];
+            }
+        }
+        return largest;
+    }
+    public static void main(String[] args) {
+        int numbers[] = {1,2,5,6,9};
+        System.out.println("largest value is : " + getLargest(numbers));
+    }
+}
