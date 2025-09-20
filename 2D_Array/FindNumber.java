3a695214244528223564a49d1d630af1ced11324
chandan9648 <chandankkumar156@gmail.com>
2025-09-20 13:14:10 +0530

    Find the number from 2D matrix

---
+++ 2D_Array/FindNumber.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+
+public class FindNumber{
+
+    public static boolean SearchTheNumber(int matrix[][], int key) {
+       
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.print("found the cell (" + i + "," + j + ") ");
+                    return true;
+                }
+               
+            }
+        }
+         System.out.print("cell not found");
+                return false;
+    }
+    
+    public static void main(String[] args) {
+        int n= 3, m = 3;
+
+        int matrix[][] =  new int[n][m];
+
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        SearchTheNumber(matrix, 5);
+    }
+}
