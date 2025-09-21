96f88b1dad28b42ef6a5d89b9fd54a9789234699
chandan9648 <chandankkumar156@gmail.com>
2025-09-21 13:09:41 +0530

    Add SearchRotatedArray class with binary search implementation

---
+++ LeetCode/SearchRotatedArray.java
@@ -0,0 +1,37 @@
+
+public class SearchRotatedArray{
+    
+    public static int rotateSorted(int arr[], int target){
+        int n = arr.length;
+          int low = 0;
+       int high = n-1;
+       while(low<high){
+           int mid = (low+high)/2;
+           
+           if(arr[mid] == target){
+               return mid;
+           } else if(arr[low] < arr[mid]){
+               if(target >= arr[low] && target <= arr[mid]){
+                   high = mid-1;
+               }else{
+                   low = mid -1;
+               }
+           } else{
+               if(target >= arr[mid] && target <= arr[high]){
+                   
+               }else{
+                   high = mid-1;
+               }
+                       
+             }
+       }
+         return -1;
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = {4, 5, 6, 7, 0, 1, 2 };
+        int target= 0;
+        rotateSorted(arr, target);
+     
+    }
+}
